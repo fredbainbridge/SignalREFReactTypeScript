047c548cf9017059fe1bb79a9241beb75db26b80
Fred Bainbridge <fred@mnscug.org>
2018-12-09 06:15:38 -0600

    changed folder structure, added readme

--- SignalRWebPackEFReactCore/Startup.cs
+++ SignalRWebPackEFReactCore/Startup.cs
@@ -33,6 +33,7 @@ namespace SignalRWebPack
                 services.AddDbContext<SignalRContext>(options =>
                         options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddSignalR();
+            services.AddMvcCore().AddJsonFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
